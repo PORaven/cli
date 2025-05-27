b794c40138990a8009ebca82711c3ea4c035a115
PORaven@Linux <SEMROM1917@outlook.com>
2025-05-28 00:41:22 +0500

    Это было мясо

---
+++ src/models/task.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import Dict
+from src.utils.task_utils import generate_GPID, generate_time
+class Task:
+    GPID : int
+    header: str
+    description: str
+    remarks: str
+    extra_status: bool
+    time_created: datetime
+
+    @classmethod
+    def create_task(cls, task: Dict):
+        return cls(
+            gpid =  generate_GPID(),
+            header = task.get("header"),
+            description = task.get("description"),
+            remarks = None,
+            extra_status = task.get("extra_status"),
+            time_created = generate_time()
+        )
+    
+    def is_valid_task(self, task) -> bool:
+        """Проверяет соответствует ли задача необходимым атрибутам
+        Принимает: Task
+        Возвращает: True - соответсвует, False - в обратном случае"""
+
+        need_attribute = ["gpid", "header", "description", "remarks", "extra_status", "time_created"]
+        for attr in need_attribute:
+            if not hasattr(task, attr):
+                return False
+        return True
